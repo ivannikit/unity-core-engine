02ba947cf0ddf7df0d81ea0af022362fc310ac06
TIGRIDO\ivan_n <ivannikit16@gmail.com>
2020-03-11 13:15:23 +0300

    add	SystemStorageInfo

---
+++ DependencyImp/SystemStorageInfo/SystemStorageInfo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace TeamZero.Core.Unity
+{
+    public static class SystemStorageInfo
+    {
+        public static ISystemStorageInfo GetDefault()
+        {
+            #if UNITY_EDITOR || UNITY_IOS
+            return new UnityApplicationStorageInfo();
+            #elif UNITY_ANDROID
+            //TODO: Override GetExternalPath
+            return new UnityApplicationStorageInfo();
+            #else
+            //TODO: LogError
+            return null;
+            #endif
+        }
+    }
+
+    public class UnityApplicationStorageInfo : ISystemStorageInfo
+    {
+        public string GetExternalPath()
+        {
+            return Application.persistentDataPath;
+        }
+
+        public string GetInternalPath()
+        {
+            return Application.persistentDataPath;
+        }
+    }
+}
