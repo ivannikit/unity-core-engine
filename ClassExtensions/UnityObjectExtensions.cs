113dd876e0538107c4d27570cf52ecce6bb0b693
Ivan <ivannikit16@gmail.com>
2020-03-13 12:21:41 +0300

    add UnityClassesExtensions

--- ClassExtensions/UnityObjectExtensions.cs
+++ ClassExtensions/UnityObjectExtensions.cs
@@ -4,17 +4,18 @@ namespace Toolbox
 {
     public static class UnityObjectExtensions
     {
-
         public static void SafeDestroy(this Object o)
         {
-#if UNITY_EDITOR
+            if(o == null) throw new System.NullReferenceException();
+
+    #if UNITY_EDITOR
             if (Application.isPlaying)
                 Object.Destroy(o);
             else
                 Object.DestroyImmediate(o);
-#else
+    #else
             Object.Destroy(o);
-#endif
+    #endif
         }
     }
 }
