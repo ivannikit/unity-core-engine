ffb013dae62acfe5a70b183b4ade8d5b75ddcc98
Ivan <ivannikit16@gmail.com>
2022-07-28 20:11:37 +0300

    add ApplicationDelegate

---
+++ Runtime/DependencyImp/ApplicationDelegate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace TeamZero
+{
+    [DisallowMultipleComponent]
+    public class ApplicationDelegate : MonoBehaviour
+    {
+        private bool _isShuttingDown = false;
+        private bool _isApplicationPaused = true;
+
+        public event Action Launch;
+        public event Action Resume;
+        public event Action Pause;
+        public event Action Quit;
+
+        private void Start()
+        {
+            OnLaunch();
+            OnResume();
+        }
+
+        private void OnApplicationPause(bool paused)
+        {
+            if (paused)
+                OnPause();
+            else
+                OnResume();
+        }
+
+        private void OnApplicationQuit()
+        {
+            if (!_isShuttingDown)
+            {
+                _isShuttingDown = true;
+                OnPause();
+                OnQuit();
+            }
+        }
+
+        private void OnDestroy() => OnPause();
+
+        private void OnLaunch() => Launch?.Invoke();
+
+        private void OnResume()
+        {
+            if (_isApplicationPaused)
+            {
+                _isApplicationPaused = false;
+                Resume?.Invoke();
+            }
+        }
+
+        private void OnPause()
+        {
+            if (!_isApplicationPaused)
+            {
+                _isApplicationPaused = true;
+                Pause?.Invoke();
+            }
+        }
+
+        private void OnQuit() => Quit?.Invoke();
+    }
+}
